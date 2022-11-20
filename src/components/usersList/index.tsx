d49fcef166382439fbe0b747e00005d87665c303
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-11-20 11:07:01 -0300

    fix: responsive design

--- src/components/usersList/index.tsx
+++ src/components/usersList/index.tsx
@@ -14,7 +14,7 @@ interface IUser {
   location: {
     city: string;
     country: string;
-  }
+  };
 }
 
 export function UsersList() {
@@ -32,38 +32,72 @@ export function UsersList() {
 
   return (
     <>
-      <div className="max-w-6xl w-full h-full mx-auto bg-gray-900 text-white">
-        <h1>Usuários</h1>
-        <table className="w-full text-center">
-          <thead>
-            <tr>
-              <th>Picture</th>
-              <th>Name</th>
-              <th>E-mail</th>
-              <th>Phone</th>
-              <th>City</th>
-              <th>Country</th>
-            </tr>
-          </thead>
-          <tbody className="">
-            {data.map((users) => (
-              <tr key={users.email}>
-                <td>
-                  <img src={users.picture.thumbnail} />
-                </td>
-                <td>
-                  {users.name.first}
-                  {users.name.last}
-                </td>
-                <td>{users.email}</td>
-                <td>{users.phone}</td>
-                <td>{users.location.city}</td>
-                <td>{users.location.country}</td>
+      <section className="">
+        <div className="max-w-4xl p-4 overflow-auto rounded-lg shadow mx-auto">
+          <div>
+          <h1>Users list</h1>
+
+          </div>
+          <table className="w-full hidden md:block">
+            <thead className="bg-gray-50 border-b-2 border-gray-200">
+              <tr>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">Picture</th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">Name</th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">E-mail</th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">Phone</th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">City</th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">Country</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody className="divide-y divide-gray-100">
+              {data.map((users) => (
+                <tr key={users.email}>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    <img src={users.picture.thumbnail} />
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {users.name.first}
+                    {users.name.last}
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">{users.email}</td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">{users.phone}</td>
+                  <td>{users.location.city}</td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">{users.location.country}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          {data.map((users) => (
+            <div className="grid grid-cols-1 gap-4 md:hidden">
+            <div className="bg-gray-600 my-2 p-4 rounded-lg shadow">
+              <div className="flex flex-wrap items-center space-x-2 text-sm">
+                <div>
+                  <img 
+                  className="max-w-[50px] max-h-[50px]"
+                  src={users.picture.thumbnail} alt={users.name.first}/>
+                </div>
+                <div>
+                  {users.name.first}{users.name.last}
+                </div>
+                <div>
+                  {users.email}
+                </div>
+                <div>
+                  {users.phone}
+                </div>
+                <div>
+                  {users.location.city}
+                </div>
+                <div>
+                  {users.location.country}
+                </div>
+              </div>
+            </div>
+          </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
