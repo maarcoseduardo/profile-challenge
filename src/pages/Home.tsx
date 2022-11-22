a98e3dd39b40faa113c56513d9dcb8603d349114
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-11-22 15:46:49 -0300

    fix: responsive homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,6 @@ import { UsersList } from "../components/usersList";
 
 export function Home() {
   return (
-    <div className="max-w-4xl w-full mx-auto p-4 overflow-auto rounded-lg shadow mt-4">
-      <UsersList />
-    </div>
+    <UsersList />
   )
 }
